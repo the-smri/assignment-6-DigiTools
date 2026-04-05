3b3cef7663b7e0fb5ab32567f3aac956c5710c5a
S M Raisul Islam <asamraisul.i@gmail.com>
2026-04-05 09:10:42 +0600

    modified StepsCards

--- src/components/homepage/getStarted/StepsCards.jsx
+++ src/components/homepage/getStarted/StepsCards.jsx
@@ -9,8 +9,8 @@ const StepsCards = ({step}) => {
           <div className="self-end flex items-center justify-center w-14 h-14 bg-linear-to-r from-[#4F39F6] to-[#9514FA] p-4 rounded-full text-white font-bold">
             {step.stepId}
           </div>
-          <div className="bg-[#E1E7FF] rounded-full p-8">
-            <img src={step.stepImageUrl} width="100px" alt="" />
+          <div className="bg-[#E1E7FF] rounded-full p-5 flex items-center justify-center">
+            <img src={step.stepImageUrl} className="w-16 h-16 object-contain" alt="" />
           </div>
           <div className="space-y-2 text-center">
             <h3 className="text-3xl font-bold text-center">{step.stepTitle}</h3>
